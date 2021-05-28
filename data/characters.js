e65bf801be99af9a73390f53d38f22eb76c192b3
Missael Ortiz <ortiz.missael80@gmail.com>
2021-05-28 14:09:10 -0700

    added classes datamodel into data/characters.js

---
+++ data/characters.js
@@ -0,0 +1,36 @@
+const Warrior = {
+    id: 1,
+    stats: {
+        class: 'Warrior',
+        health: 10,
+        maxHealth: 10,
+        attack: 5,
+        ac: 8,
+        gold: 5,
+        xp: 0
+    }
+};
+const Bard = {
+    id: 2,
+    stats: {
+        class: 'Bard',
+        health: 8,
+        maxHealth: 8,
+        attack: 3,
+        ac: 6,
+        gold: 10,
+        xp: 0
+    }
+};
+const Mage = {
+    id: 3,
+    stats: {
+        class: 'Mage',
+        health: 5,
+        maxHealth: 5,
+        attack: 10,
+        ac: 3,
+        gold: 8,
+        xp: 0
+    }
+};
