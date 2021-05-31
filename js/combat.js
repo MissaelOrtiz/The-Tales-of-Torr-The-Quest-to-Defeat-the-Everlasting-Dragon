c2238bc76b7cbf841d133e32b6bab9670b38b989
DJ Drakos <dianajodrakos@gmail.com>
2021-05-31 13:43:58 -0700

    built doesAttackHit, grantRewards, and flee functions

--- js/combat.js
+++ js/combat.js
@@ -3,6 +3,28 @@ export function combatAttackRoll() {
     return rndmRoll;
 }
 
+export function doesAttackHit(attacker, defender, attackRoll) {
+    if (attackRoll + attacker.stats.attack >= defender.stats.ac) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 export function dealDamage(attacker, defender) {
     defender.stats.health = defender.stats.health - attacker.stats.attack;
 }
+
+export function grantRewards(user, enemy) {
+    user.stats.gold = user.stats.gold + enemy.stats.gold;
+    user.stats.xp = user.stats.xp + enemy.stats.xp;
+}
+
+export function flee(user, enemy) {
+    if (user.stats.gold < enemy.stats.gold) {
+        return alert('You are too poor to flee! You must fight!');
+    } else {
+        user.stats.gold = user.stats.gold - enemy.stats.gold;
+    }
+}
+
