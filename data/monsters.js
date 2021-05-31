32f85486bbde4450e81a952d24017690c90b1cc2
Missael Ortiz <ortiz.missael80@gmail.com>
2021-05-31 13:21:06 -0700

    started combat logic on js/combat.js

--- data/monsters.js
+++ data/monsters.js
@@ -1,52 +1,67 @@
 const goblin = {
     id: 1,
-    level: 1,
-    name: 'Goblin',
-    health: 5,
-    attack: 2,
-    ac: 2,
-    gold: 3,
-    xp: 4 
+    stats: {
+        level: 1,
+        name: 'Goblin',
+        health: 5,
+        maxHealth: 5,
+        attack: 2,
+        ac: 2,
+        gold: 3,
+        xp: 4
+    } 
 };
 const wolf = {
     id: 2,
-    level: 1,
-    name: 'Timber Wolf',
-    health: 7,
-    attack: 4,
-    ac: 5,
-    gold: 10,
-    xp: 6 
+    stats: {
+        level: 1,
+        name: 'Timber Wolf',
+        health: 7,
+        maxHealth: 7,
+        attack: 4,
+        ac: 5,
+        gold: 10,
+        xp: 6 
+    }
 };
 const bandit = {
     id: 3,
-    level: 1,
-    name: 'Newbie Bandit',
-    health: 8,
-    attack: 3,
-    ac: 4,
-    gold: 5,
-    xp: 6 
+    stats: {
+        level: 1,
+        name: 'Newbie Bandit',
+        health: 8,
+        maxHealth: 8,
+        attack: 3,
+        ac: 4,
+        gold: 5,
+        xp: 6
+    } 
 };
 const slime = {
     id: 4,
-    level: 1,
-    name: 'Common Forest Slime',
-    health: 14,
-    attack: 2,
-    ac: 1,
-    gold: 2,
-    xp: 6 
+    stats: {
+        level: 1,
+        name: 'Common Forest Slime',
+        health: 14,
+        maxHealth: 14,
+        attack: 2,
+        ac: 1,
+        gold: 2,
+        xp: 6 
+    }
 };
 const abomination = {
     id: 5,
-    level: 2,
-    name: 'Oh Gods, What is That',
-    health: 15,
-    attack: 3,
-    ac: 4,
-    gold: 0,
-    xp: 10 
+    stats: {
+        level: 2,
+        name: 'Oh Gods, What is That',
+        health: 15,
+        maxHealth: 15,
+        attack: 3,
+        ac: 4,
+        gold: 0,
+        xp: 10 
+    }
 };
 
-export default [goblin, wolf, bandit, slime, abomination]
+export default [goblin, wolf, bandit, slime, abomination];
