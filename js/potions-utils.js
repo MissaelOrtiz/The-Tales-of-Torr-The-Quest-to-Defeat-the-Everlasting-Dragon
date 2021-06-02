19630c52eae2d1ff34c4a8113c8df993b6fb6c4c
Sean Simpson <simpson.sean@outlook.com>
2021-06-02 14:21:37 -0700

    Merchant and Potions work

---
+++ js/potions-utils.js
@@ -0,0 +1,9 @@
+//import functions and utilities
+import { findById } from './utils.js';
+import { }
+
+
+
+//set DOM and CONST
+
+//event listening
---
+++ village/Merchant/app.js
@@ -0,0 +1,23 @@
+//import
+import { updateCombatLog } from '../../js/combat.js';
+import { getCurrentUser, setUser } from '../../js/local-storage-utils.js';
+
+//set DOMS and CONSTs
+const mercButtVillage = document.querySelector('.merc-butt-village');
+const mercButtTalk = document.querySelector('.merc-butt-talk');
+const mercButtBuy = document.querySelector('.merc-butt-buy');
+const botCenter = document.querySelector('.bot-center');
+
+const user = getCurrentUser();
+
+//event listeners
+mercButtVillage.addEventListener('click', () => {
+    window.location = '../';
+
+});
+
+mercButtTalk.addEventListener('click', () => {
+    const chatter = updateCombatLog(`hello ${user.hero}.`);
+    botCenter.append(chatter);
+});
+
